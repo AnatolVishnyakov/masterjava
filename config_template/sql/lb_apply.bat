fd82c4fcade62e300944e31aade66114931206f2
Anatol V <malfenius@gmail.com>
2020-07-15 17:33:04 +0300

    HW-6. Patch 6.1. Homework. Create data model

---
+++ config_template/sql/lb_apply.bat
@@ -0,0 +1,9 @@
+set JAVA_HOME=
+set LB_HOME=
+call %LB_HOME%\liquibase.bat --driver=org.postgresql.Driver ^
+--classpath=%LB_HOME%\lib ^
+--changeLogFile=databaseChangeLog.sql ^
+--url="jdbc:postgresql://localhost:5432/masterjava" ^
+--username=user ^
+--password=password ^
+migrate
